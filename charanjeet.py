851cd9090fa6747419d702113a8aa9a46c1939dc
beyondurexpectation <109571396+beyondurexpectation@users.noreply.github.com>
2024-01-26 20:49:50 +0530

    Update charanjeet.py

--- charanjeet.py
+++ charanjeet.py
@@ -6,8 +6,8 @@ new_data=st.experimental_data_editor(data,num_rows="dynamic")
 
 
 final_data=pd.DataFrame(new_data)
-final_data["Order Qty(MT)"]=final_data["Order Qty(MT)"].astype(str).astype(int)
-final_data["Dispatch Qty(MT)"]=final_data["Dispatch Qty(MT)"].astype(str).astype(int)
+final_data["Order Qty(MT)"]=final_data["Order Qty(MT)"]
+final_data["Dispatch Qty(MT)"]=final_data["Dispatch Qty(MT)"]
 
 final_data["Pending Qty"]=(final_data["Order Qty(MT)"])-(final_data["Dispatch Qty(MT)"])
 st.write(final_data)
